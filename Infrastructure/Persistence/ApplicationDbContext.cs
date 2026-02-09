8fc23470b651c858595cd8f9898ad36a415e3b17
joshua <3729344@myuwc.ac.za>
2026-02-09 17:16:09 +0200

    added on model creating

--- Infrastructure/Persistence/ApplicationDbContext.cs
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -2,12 +2,14 @@
 using HavenSync_api.Infrastructure.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using HavenSync_api.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Property = HavenSync_api.Domain.Entities.Property;
 
 
-namespace HavenSync_api.Infrastructure.Persistence 
+namespace HavenSync_api.Infrastructure.Persistence
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
     {
@@ -19,5 +21,21 @@ namespace HavenSync_api.Infrastructure.Persistence
         public DbSet<Property> Properties => Set<Property>();
         public DbSet<Unit> Units => Set<Unit>();
         public DbSet<Lease> Leases => Set<Lease>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Lease>()
+                .HasOne(l => l.Property)
+                .WithMany(p => p.Leases)
+                .HasForeignKey(l => l.PropertyId);
+
+            builder.Entity<Lease>()
+               .HasOne(l => l.Property)
+               .WithMany(p => p.Leases)
+               .HasForeignKey(l => l.PropertyId);
+
+        }
     }
 }
