deac0d74f580c3520f3ff450ac70ff31aca300aa
joshua <3729344@myuwc.ac.za>
2026-02-11 15:43:20 +0200

    update post property

--- API/Controllers/LandlordController.cs
+++ API/Controllers/LandlordController.cs
@@ -19,20 +19,49 @@ namespace HavenSync_api.API.Controllers
         }
 
         [HttpPost("properties")]
-        public async Task<IActionResult> AddProperty(Property property)
+        public async Task<IActionResult> AddProperty([FromBody] CreatePropertyDto request)
         {
-            property.LandlordId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+            var landlordId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (landlordId == null) return Unauthorized();
+
+            var property = new Property
+            {
+                Id = Guid.NewGuid(),
+                LandlordId = Guid.Parse(landlordId),
+
+                Name = request.Name,
+                StreetAddress = request.StreetAddress,
+                Suburb = request.Suburb,
+                City = request.City,
+                Province = request.Province,
+                PostalCode = request.PostalCode,
+
+                PropertyType = request.PropertyType,
+                Bedrooms = request.Bedrooms,
+                Bathrooms = request.Bathrooms,
+                ParkingSpaces = request.ParkingSpaces,
+                FloorSizeSqm = request.FloorSizeSqm,
+                ErfSizeSqm = request.ErfSizeSqm,
+                IsFurnished = request.IsFurnished,
+
+                MonthlyRent = request.MonthlyRent,
+                DepositAmount = request.DepositAmount,
+                AvailableFrom = request.AvailableFrom,
+                UtilitiesIncluded = request.UtilitiesIncluded,
+                PrepaidElectricity = request.PrepaidElectricity,
+
+                LevyAmount = request.LevyAmount,
+                RatesAndTaxes = request.RatesAndTaxes,
+                MunicipalAccountNumber = request.MunicipalAccountNumber,
+                TitleDeedNumber = request.TitleDeedNumber,
+                IsSectionalTitle = request.IsSectionalTitle,
+                HasHOA = request.HasHOA
+            };
+
             _context.Properties.Add(property);
             await _context.SaveChangesAsync();
-            return Ok(property);
-        }
 
-        [HttpGet("properties")]
-        public IActionResult GetProperties()
-        {
-            var landlordId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
-            var props = _context.Properties.Where(p => p.LandlordId == landlordId).ToList();
-            return Ok(props);
+            return Ok(property);
         }
     }
 }
